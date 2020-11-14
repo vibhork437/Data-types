f9d3d9f806dd8b94f0f1b921eba57a21e4a48abf
vibhork437 <65237757+vibhork437@users.noreply.github.com>
2020-11-14 12:25:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+console.log("Data Types")
+
+/*Primary Data types
+
+1. String 
+2. Number
+3. Boolean
+4. null
+5. Undefined
+6. Symbol - introduced recently
+
+*/
+
+//String
+/*Anything written in 
+semicolon is string
+*/
+
+const name = "vibhor"
+console.log(name)
+
+//number
+
+const number = 34
+console.log(number)
+
+//Boolean - true/false
+
+const boolean = true
+console.log(boolean)
+
+//null
+
+const rem = null
+console.log(rem);
+
+//undefined
+
+var undef;
+
+console.log(undef)
+
+/*
+typeof is a command which helps us
+to identify Data types
+*/
+
+const example = 34
+console.log(example + typeof(example))
+
+//Refrence data types
+
+//Array
+
+const array = [3,4,4,5]
+console.log(array + typeof(array))
+
+
+//Object literal
+
+let Ryuk = {
+    Rem:32,
+    yagami:44,
+    Lawlight:40
+}
+
+console.log(Ryuk)
+
+function fame(){
+
+}
+
+console.log(fame + typeof(fame))
+
+//Dates
+
+var date = new Date
+
+console.log(date + typeof(date))
